4c3c6837b8564b6bc9bccb625e925b3b8fe8a9e3
Evgeny S <j3000@bk.ru>
2024-03-15 22:50:39 +0300

    feat: add work with file to SourceService

--- src/main/java/ru/bk/j3000/normarchivedata/service/SourceService.java
+++ src/main/java/ru/bk/j3000/normarchivedata/service/SourceService.java
@@ -1,4 +1,12 @@
 package ru.bk.j3000.normarchivedata.service;
 
+import ru.bk.j3000.normarchivedata.model.Source;
+
+import java.io.File;
+import java.util.List;
+
 public interface SourceService {
+    List<Source> readSrcFromFile(File file);
+
+    File saveSrcToFile(List<Source> sources);
 }
--- src/main/java/ru/bk/j3000/normarchivedata/service/SourceServiceImpl.java
+++ src/main/java/ru/bk/j3000/normarchivedata/service/SourceServiceImpl.java
@@ -2,11 +2,24 @@ package ru.bk.j3000.normarchivedata.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.bk.j3000.normarchivedata.model.Source;
 import ru.bk.j3000.normarchivedata.repository.SourceRepository;
+
+import java.io.File;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SourceServiceImpl implements SourceService {
     private final SourceRepository sourceRepository;
 
+    @Override
+    public List<Source> readSrcFromFile(File file) {
+        return null;
+    }
+
+    @Override
+    public File saveSrcToFile(List<Source> sources) {
+        return null;
+    }
 }
