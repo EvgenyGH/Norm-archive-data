427e2289e696f47c459b06e024d3d97d2cbce4c1
J3000 <j3000@bk.ru>
2024-04-09 10:33:10 +0300

    feat: add tests to SourceService

--- src/test/java/ru/bk/j3000/normarchivedata/service/SourceServiceImplTest.java
+++ src/test/java/ru/bk/j3000/normarchivedata/service/SourceServiceImplTest.java
@@ -1,29 +1,86 @@
 package ru.bk.j3000.normarchivedata.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import ru.bk.j3000.normarchivedata.model.SOURCE_TYPE;
+import ru.bk.j3000.normarchivedata.model.Source;
 
 import java.io.File;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Transactional
 public class SourceServiceImplTest {
     private final SourceServiceImpl service;
+    private final File file = new File("src/test/resources/testdata/sources.xlsm");
 
     @Test
-    @DisplayName("Read sources from file")
-    public void whenReadSourcesFromFileThenGetSourcesCollection() {
-        File file = new File("src/test/resources/testdata/sources.xlsm");
-
+    @DisplayName("Upload sources from file")
+    public void whenReadSourcesFromFileAndGetSourcesFromDataBaseThenSourcesNumberIsEqual() {
         service.uploadSources(file);
 
         assertThat(service.getAllSources().size()).isEqualTo(141);
+    }
+
+    @Test
+    @DisplayName("Save sources")
+    public void whenSaveSourcesToDatabaseAndGetSourcesFromDataBaseThenSourcesNumberIsEqual() {
+        List<Source> sources = List.of(new Source(null, "name1", "address1", SOURCE_TYPE.KTS),
+                new Source(null, "name1", "address1", SOURCE_TYPE.RTS));
+        service.saveSources(sources);
+
+        assertThat(service.getAllSources().size()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("Save source")
+    public void whenSaveSourceToDatabaseAndGetSourceFromDataBaseThenSourceIsOneAndNameIsEqual() {
+        Source source = new Source(null, "name1", "address1", SOURCE_TYPE.KTS);
+        service.saveSource(source);
+
+        assertThat(service.getAllSources().size()).isEqualTo(1);
+        assertThat(service.getAllSources().getFirst().getName()).isEqualTo(source.getName());
+    }
+
+    @Test
+    @DisplayName("Delete all sources")
+    public void whenAddTwoSourcesToDatabaseAndDeleteAllSourcesFromDataBaseThenSourcesNumberNull() {
+        List<Source> sources = List.of(new Source(null, "name1", "address1", SOURCE_TYPE.KTS),
+                new Source(null, "name1", "address1", SOURCE_TYPE.RTS));
+        service.saveSources(sources);
+        service.deleteAllSources();
+
+        assertThat(service.getAllSources()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Delete sources by ids")
+    public void whenSaveSourceToDatabaseAndDeleteSourceFromDataBaseByIdThenSourceNumberIsNull() {
+        Source source = new Source(null, "name1", "address1", SOURCE_TYPE.KTS);
+        service.saveSource(source);
+        service.deleteSourcesById(Collections.singletonList(source.getId()));
+
+        assertThat(service.getAllSources()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Update source")
+    public void whenSaveSourceToDatabaseAndUpdateSourceNameThenSourceNameIsUpdated() {
+        Source source = new Source(null, "name1", "address1", SOURCE_TYPE.KTS);
+        service.saveSource(source);
+        source.setName("updatedName");
+        service.updateSource(source);
+
+        assertThat(service.getAllSources().getFirst().getName()).isEqualTo("updatedName");
     }
 }
