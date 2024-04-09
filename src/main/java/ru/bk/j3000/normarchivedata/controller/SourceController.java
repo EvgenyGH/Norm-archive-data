0ed0f7fcc4ba7a72d44fb779d04bfae12ad0fc10
J3000 <j3000@bk.ru>
2024-04-09 17:11:03 +0300

    feat: add view 1

--- src/main/java/ru/bk/j3000/normarchivedata/controller/SourceController.java
+++ src/main/java/ru/bk/j3000/normarchivedata/controller/SourceController.java
@@ -1,17 +1,19 @@
 package ru.bk.j3000.normarchivedata.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import ru.bk.j3000.normarchivedata.service.SourceService;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class SourceController {
     private final SourceService sourceService;
 
     @GetMapping("/welcome")
-    public String welcome() {
+    public String welcome(Model model) {
+        model.addAttribute("name", "World!");
         return "welcome";
     }
 }
