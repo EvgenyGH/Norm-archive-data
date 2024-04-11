63ecb7f37d2d985c1961dc7abc801bd4dc6ce545
J3000 <j3000@bk.ru>
2024-04-11 16:35:10 +0300

    feat: add security configuration

---
+++ src/main/java/ru/bk/j3000/normarchivedata/config/SecurityConfiguration.java
@@ -0,0 +1,76 @@
+package ru.bk.j3000.normarchivedata.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetails(PasswordEncoder encoder) {
+        var manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("admin")
+                .password(encoder.encode("12345"))
+                .roles("ADMIN")
+                .build());
+        manager.createUser(User.withUsername("user")
+                .password(encoder.encode("54321"))
+                .roles("USER")
+                .build());
+        return manager;
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+
+        http.formLogin(conf -> conf
+                        .defaultSuccessUrl("/welcome"))
+                .logout(config -> config
+                        .clearAuthentication(true)
+                        .deleteCookies("JSESSIONID"));
+
+        http.csrf(Customizer.withDefaults());
+        http.cors(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(conf ->
+                conf
+                        .requestMatchers("/welcome/**",
+                                "/source/**",
+                                "/property/**",
+                                "/zone/**",
+                                "/ssfc/**",
+                                "/report/**",
+                                "/css/*")
+                        .hasRole("USER")
+                        .requestMatchers("/user")
+                        .hasRole("ADMIN")
+                        .anyRequest()
+                        .denyAll()
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    RoleHierarchy roleHierarchy() {
+        var hierarchy = new RoleHierarchyImpl();
+        hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        return hierarchy;
+    }
+}
