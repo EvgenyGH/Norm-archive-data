9d0bebfebee8833c641de374531120b0721cbce1
Evgeny S <j3000@bk.ru>
2024-05-15 00:02:17 +0300

    feat: add all source report 3

--- src/main/java/ru/bk/j3000/normarchivedata/service/ReportServiceImpl.java
+++ src/main/java/ru/bk/j3000/normarchivedata/service/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package ru.bk.j3000.normarchivedata.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.apache.poi.ss.formula.eval.NotImplementedException;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellUtil;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -24,6 +26,7 @@ public class ReportServiceImpl implements ReportService {
             "Тип источника"};
     private final SourceService sourceService;
 
+
     @Override
     public Resource getAllSourcesReport() {
         Resource resource;
@@ -33,32 +36,13 @@ public class ReportServiceImpl implements ReportService {
 
 
         try (Workbook wb = new XSSFWorkbook()) {
-            CellStyle headerPrimary = wb.createCellStyle();
-            //alignment
-            headerPrimary.setAlignment(HorizontalAlignment.CENTER);
-            headerPrimary.setVerticalAlignment(VerticalAlignment.CENTER);
-            //border
-            headerPrimary.setBorderBottom(BorderStyle.THIN);
-            headerPrimary.setBorderLeft(BorderStyle.THIN);
-            headerPrimary.setBorderRight(BorderStyle.THIN);
-            headerPrimary.setBorderTop(BorderStyle.THIN);
-            headerPrimary.setLeftBorderColor(IndexedColors.BLACK.getIndex());
-            headerPrimary.setRightBorderColor(IndexedColors.BLACK.getIndex());
-            headerPrimary.setTopBorderColor(IndexedColors.BLACK.getIndex());
-            headerPrimary.setBottomBorderColor(IndexedColors.BLACK.getIndex());
-            //fill
-            //headerPrimary.setFillForegroundColor(new XSSFColor(new java.awt.Color(255, 204, 153), new DefaultIndexedColorMap()));
-            headerPrimary.setFillForegroundColor(IndexedColors.TAN.getIndex());
-            headerPrimary.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-            //font
             Font font = wb.createFont();
-            font.setFontHeightInPoints((short) 12);
-            font.setFontName("Times New Roman");
-            font.setBold(true);
-            font.setColor(IndexedColors.BLACK.getIndex());
-            headerPrimary.setFont(font);
-            //
 
+            CellStyle headerStylePrimary = getPrimaryHeaderStyle(wb.createCellStyle(), font);
+            CellStyle headerStyleSecondary = getSecondaryHeaderStyle(wb.createCellStyle(), font);
+            CellStyle stringStyle = getStringStyle(wb.createCellStyle(), font);
+            CellStyle integerStyle = getIntegerStyle(wb.createCellStyle(), font);
+            CellStyle decimalStyle = getDecimalStyle(wb.createCellStyle(), font, 3);
 
             // DataFormatter df = new DataFormatter();
             // CreationHelper helper = wb.getCreationHelper();
@@ -68,23 +52,18 @@ public class ReportServiceImpl implements ReportService {
             // CellRefUtil
             // sheet.addMergedRegion(new CellRangeAddress(1, //first row (0-based)1, //last row  (0-based)1, //first column (0-based)2  //last column  (0-based)));
 
-            CellStyle headerSecondary = wb.createCellStyle();
-            CellStyle cellText = wb.createCellStyle();
-            CellStyle cellNumber = wb.createCellStyle();
-            CellStyle cellDecimal = wb.createCellStyle();
-
-
             Sheet sheet = wb.createSheet("Sources");
             Row row = sheet.createRow(0);
-            row.createCell(5).setCellValue("UUID7");
-            row.setRowStyle(headerPrimary);
 
-
-            //set headers
+            //setup headers
             IntStream.rangeClosed(0, allSrcColumns.length - 1)
-                    .forEach(i -> row.createCell(i, CellType.STRING).setCellValue(allSrcColumns[i]));
-            // set header style
+                    .forEach(i -> CellUtil.createCell(row, i, allSrcColumns[i], headerStylePrimary));
 
+            // autosize columns
+            IntStream.rangeClosed(0, allSrcColumns.length - 1)
+                    .forEach(sheet::autoSizeColumn);
+
+            sheet.autoSizeColumn(2);
 
             FileOutputStream outputStream = new FileOutputStream(fileLocation);
             wb.write(outputStream);
@@ -98,6 +77,74 @@ public class ReportServiceImpl implements ReportService {
         log.info("All sources report created.");
 
         return resource;
+    }
+
+    private CellStyle getIntegerStyle(CellStyle cellStyle, Font font) {
+        getBaseStyle(cellStyle, font)
+                .setDataFormat((short) BuiltinFormats.getBuiltinFormat("# ##0"));
+
+        return cellStyle;
+    }
+
+    private CellStyle getDecimalStyle(CellStyle cellStyle, Font font, Integer digits) {
+        getBaseStyle(cellStyle, font)
+                .setDataFormat((short) BuiltinFormats
+                        .getBuiltinFormat(String.format("# ##0.%s", Strings.repeat("0", digits)))
+                );
+
+        return cellStyle;
+    }
+
+    private CellStyle getStringStyle(CellStyle cellStyle, Font font) {
+        getBaseStyle(cellStyle, font)
+                .setDataFormat((short) BuiltinFormats.getBuiltinFormat("@"));
+
+        return cellStyle;
+    }
+
+    private CellStyle getSecondaryHeaderStyle(CellStyle cellStyle, Font font) {
+        font.setBold(true);
+        getStringStyle(cellStyle, font);
+        //fill
+        cellStyle.setFillForegroundColor(IndexedColors.TAN.getIndex());
+        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        return cellStyle;
+    }
+
+    private CellStyle getPrimaryHeaderStyle(CellStyle cellStyle, Font font) {
+        font.setBold(true);
+        getStringStyle(cellStyle, font);
+        //fill
+        //headerPrimary.setFillForegroundColor(new XSSFColor(new java.awt.Color(255, 204, 153), new DefaultIndexedColorMap()));
+        cellStyle.setFillForegroundColor(IndexedColors.LEMON_CHIFFON.getIndex());
+        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        return cellStyle;
+    }
+
+    private CellStyle getBaseStyle(CellStyle cellStyle, Font font) {
+        //alignment
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+
+        //border
+        cellStyle.setBorderBottom(BorderStyle.THIN);
+        cellStyle.setBorderLeft(BorderStyle.THIN);
+        cellStyle.setBorderRight(BorderStyle.THIN);
+        cellStyle.setBorderTop(BorderStyle.THIN);
+        cellStyle.setLeftBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyle.setRightBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyle.setTopBorderColor(IndexedColors.BLACK.getIndex());
+        cellStyle.setBottomBorderColor(IndexedColors.BLACK.getIndex());
+
+        //font
+        font.setFontHeightInPoints((short) 12);
+        font.setFontName("Times New Roman");
+        font.setColor(IndexedColors.BLACK.getIndex());
+        cellStyle.setFont(font);
+
+        return cellStyle;
     }
 
     @Override
